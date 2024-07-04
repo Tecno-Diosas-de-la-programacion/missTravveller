6d97a18bb4ae7b115f42a09b3388368351a8a623
Arantxa Vazquez <arantxavazquez177@gmail.com>
2024-07-04 17:34:27 -0600

    add: navegacion entre todas las paginas

---
+++ src/pages/nosotros/paginaNosotros.js
@@ -0,0 +1,8 @@
+import './styles.scss'
+import './paginaNosotros.css'
+
+//import './src/components/navbar/navbar-app'
+import { navbarApp } from '../../components/navbar/navbar-app.js'
+
+
+document.querySelector("#navbar-app").innerHTML= navbarApp();
