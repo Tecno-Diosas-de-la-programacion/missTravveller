c7e810c77934e1e716fcfa833fe1efc2f3b827a6
Jaqueline <jaqueline.torres.perez.unam@gmail.com>
2024-07-07 19:04:24 -0600

    add: cambios para que funcioné la pagina productos forma producción

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 import './styles.scss'
 import './src/pages/nosotros/paginaNosotros.css'
-import './src/pages/contacto/paginaContacto.js'
+//import './src/pages/contacto/paginaContacto.js'
 
 import './src/components/navbar/navbar-app'
 import { navbarApp } from './src/components/navbar/navbar-app.js'
+
+//import './src/pages/productos/getProducts.js'
+//import './src/pages/contacto/validacionFormulario.js'
 
 document.querySelector("#navbar-app").innerHTML= navbarApp();
 
