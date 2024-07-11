3ece6f67b19d2baca567a50417da04af07b4f7f6
Diana Gallegos <diana_prz21@hotmail.com>
2024-07-11 00:26:36 -0600

    Create: primer modelo de formulario sin validaciones

--- src/components/navbar/navbar-app.js
+++ src/components/navbar/navbar-app.js
@@ -6,12 +6,12 @@ const navbarApp = () => {
   <nav class="navbar navbar-expand-lg py-0">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
-        <img src="/public/img/Logo_MissTravveller.png" alt="Logo" width="75" height="75"
+        <img src="/img/Logo_MissTravveller.png" alt="Logo" width="75" height="75"
           class="d-inline-block align-text-center">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <img id="hamburguesa" src="/public/img/hamburguesa.png" alt="Imagen patito despliega menú">
+        <img id="hamburguesa" src="/img/hamburguesa.png" alt="Imagen patito despliega menú">
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
