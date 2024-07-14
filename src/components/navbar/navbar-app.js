39d6f22629fffdb2c192cc93f78f018db711b4cb
Jaqueline <jaqueline.torres.perez.unam@gmail.com>
2024-07-14 14:10:00 -0600

    update: se cambio la ruta del navbar del index.html

--- src/components/navbar/navbar-app.js
+++ src/components/navbar/navbar-app.js
@@ -16,7 +16,7 @@ const navbarApp = () => {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/src/index.html">Inicio</a>
+            <a class="nav-link" aria-current="page" href="/index.html">Inicio</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/src/pages/nosotros/paginaNosotros.html">Nosotros</a>
