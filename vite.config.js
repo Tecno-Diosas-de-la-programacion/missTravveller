de57cf19a4cab2e6f58a29acd16c5018d90bf0c0
Diana Gallegos <diana_prz21@hotmail.com>
2024-07-14 21:58:40 -0600

    Add: funcionalidad para usar el preview

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         form: resolve(__dirname, 'src/pages/formulario/formulario.html'),
         products: resolve(__dirname, 'src/pages/productos/productos.html'),
         signup: resolve(__dirname, 'src/pages/registro/registro.html'),
+        signin: resolve(__dirname, 'src/pages/ingresar/ingresar.html')
 
       },
     },
