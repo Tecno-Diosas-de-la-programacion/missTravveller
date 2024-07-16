e408f28be56cc62714e220eb4ce979f1e934ac17
Abigail Ramirez <abigailrg03@gmail.com>
2024-07-15 19:08:49 -0600

    adicion de funciones en localStorage y Admin

--- src/pages/formularioAdmin/postLocalStorage.js
+++ src/pages/formularioAdmin/postLocalStorage.js
@@ -27,4 +27,29 @@ const postContactForm = async () =>{
     console.log(contactResults);
 
     return contactResults
-}                                            
+} 
+
+
+//Aqui empieza la adicion de productos con la funcion postProduct=============================
+function postProduct (newProduct){
+    fetch("/public/db.json")
+        .then((resolve) => resolve.json())
+        .then((data) => {
+            console.log(data);
+            //Variable temporal para guardar el producto nuevo, para no modificar el original
+            const tempProducts = data;
+            //Aqui incrementamos el id y obtenemos el ultimo elemento
+            const lastId = tempProducts[tempProducts.length-1].id;
+            newProduct.id = lastId + 1;
+            //aqui colocamos el ultimo objeto al json
+            tempProducts.push(newProduct);
+            //Vamos a guardar el nuevo producto en el db2.json temporal, el writeFile no funciona
+            writeFile("/public/db2.json", JSON.stringify(tempProducts, null, 4));
+            //writeFile("/public/db2.json", JSON.stringify(tempProducts, null, 4));
+            })
+        .catch((error) => {
+        console.log(error);
+});
+
+}
+
