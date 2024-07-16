848e070e651f191ec2601107020b017e51167066
Fanny Contreras <fanny7cg@gmail.com>
2024-07-15 20:48:35 -0600

    Se agregaron: las validaciones a cada input del formulario administrativo, para que al llenarlo marque errores

--- src/pages/formularioAdmin/formularioAdmin.js
+++ src/pages/formularioAdmin/formularioAdmin.js
@@ -13,105 +13,105 @@ const contactForm = document.forms["productForm"];
  * cambios de teclado, movimientos del mouse, entre otros.
  */
 // contactForm.addEventListener( "click"  , ()=>{} );
-productForm.addEventListener( "button"  , async ( event )=>{
-    event.preventDefault();
-    console.log("Estoy en el evento click del botón");
+productForm.addEventListener("button", async (event) => {
+  event.preventDefault();
+  console.log("Estoy en el evento click del botón");
 
-// Referencia de inputs TERMINAR ARANTXA Y ABI
-const nombreDestinoRef = productForm.elements["nombre-destino"];
-const fechaRef = productForm.elements["fecha"];
-const precioRef = productForm.elements["precio"];
-const archivoRef = productForm.elements["file-upload"];
-const descripcionRef = productForm.elements["descripcion"];
-const cupoRef = productForm.elements["cupo"]; 
-//ESTOS SOON DEL ITINERARIO
-const puntoReunionRef = productForm.elements["punto-reunion"];
-const horaSalidaRef = productForm.elements["hora-salida"];
-const descripcionActRef = productForm.elements["descripcion-actividad"];
-const horasEstRef = productForm.elements["hora-estadia"];
-const horaRegresoRef = productForm.elements["hora-regreso"]; 
-
-
-// Sanitizar los datos ¿como podemos limpiar los elementosya recibidos? ARANTXA Y ABI
-emailRef.value = emailRef.value.toLowerCase();
-fullNameRef.value = fullNameRef.value.trim();
+  // Referencia de inputs TERMINAR ARANTXA Y ABI
+  const nombreDestinoRef = productForm.elements["nombre-destino"];
+  const fechaRef = productForm.elements["fecha"];
+  const precioRef = productForm.elements["precio"];
+  const descripcionRef = productForm.elements["descripcion"];
+  const cupoRef = productForm.elements["cupo"];
+  //ESTOS SON DEL ITINERARIO
+  const puntoReunionRef = productForm.elements["punto-reunion"];
+  const horaSalidaRef = productForm.elements["hora-salida"];
+  const descripcionActRef = productForm.elements["descripcion-actividad"];
+  const horasEstRef = productForm.elements["hora-estadia"];
+  const horaRegresoRef = productForm.elements["hora-regreso"];
 
 
+  // Sanitizar los datos ¿como podemos limpiar los elementosya recibidos? ARANTXA Y ABI
+  emailRef.value = emailRef.value.toLowerCase();
+  fullNameRef.value = fullNameRef.value.trim();
 
-const formData = {
-    id : 0, //funcion de abi 
-    nombreDestino : nombreDestinoRef.value,
-    fechaViaje : fechaRef.value, 
+
+
+  const formData = {
+    id: 0, //funcion de abi 
+    nombreDestino: nombreDestinoRef.value,
+    fechaViaje: fechaRef.value,
     precio: precioRef.value,
-    img : archivoRef.value,
-    descripcion : descripcionRef.value,
-    cupo : cupoRef.value, 
-    itinerario : [{
-        puntoReunion : puntoReunionRef.value, 
-        horaSalida : horaSalidaRef.value,
-        actividad : descripcionActRef.value, //descripcion de la actividad
-        horasEstadia : horasEstRef.value,
-        horaRegreso : horaRegresoRef.value 
+    img: archivoRef.value,
+    descripcion: descripcionRef.value,
+    cupo: cupoRef.value,
+    itinerario: [{
+      puntoReunion: puntoReunionRef.value,
+      horaSalida: horaSalidaRef.value,
+      actividad: descripcionActRef.value, //descripcion de la actividad
+      horasEstadia: horasEstRef.value,
+      horaRegreso: horaRegresoRef.value
     }
-    ], 
-    
- }
- console.table( formData );
+    ],
 
- //Insertar nuestras validaciones FANNY Y DULCE
+  }
+  console.table(formData);
 
- let isValid = true;
-    let alertContainer = document.getElementById('alert-container');
-    alertContainer.innerHTML = '';
+  //Insertar nuestras validaciones FANNY Y DULCE
+
+  let isValid = true;
+  let alertContainer = document.getElementById('alert-container');
+  alertContainer.innerHTML = '';
 
 
-    // Validate Nombre destino
 
- const validateInputsForm = (formData) => {
+  // Validate Nombre destino
+
+  const validateInputsForm = (formData) => {
     const results = {
       isValid: true,
       error: "",
     };
-  
-    const checkFullName = validateFullName(formData.fullName) 
-    if (  checkFullName.isValid === false ){
+
+    const checkFullName = validateFullName(formData.fullName)
+    if (checkFullName.isValid === false) {
       results.isValid = false;
       results.error = checkFullName.error;
     }
-  
+
     return results;
   };
+
+
+  const results = validateInputsForm(formData);
+
+  if (results.isValid) {
+    try {
+      await localStorage(); // ARANTXA ABI
+    }
+    catch (error) {
+      const errorMessage = document.getElementById("post-error-message");
+      errorMessage.innerHTML = error;
+      errorMessage.style.display = "block";
+      setTimeout(() => errorMessage.style.display = "none", 5000);
+    }
+  } else {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.innerHTML = results.error;
+    errorMessage.style.display = "block";
+    setTimeout(() => errorMessage.style.display = "none", 5000);
+  };
+
+
+
+  /*Pasos para realizar
   
-
- const results = validateInputsForm(formData);
-
- if( results.isValid ){
-    try{
-        await localStorage(); // ARANTXA ABI
-    }
-    catch(error){
-        const errorMessage = document.getElementById("post-error-message");
-   errorMessage.innerHTML = error;
-   errorMessage.style.display = "block";
-   setTimeout( ()=> errorMessage.style.display = "none", 5000  );
-    }
- }else{
-   const errorMessage = document.getElementById("error-message");
-   errorMessage.innerHTML = results.error;
-   errorMessage.style.display = "block";
-   setTimeout( ()=> errorMessage.style.display = "none", 5000  );
- };
-
-
-
-/*Pasos para realizar
-
-1. Obetenr datos del formulario cada uno corresponde a una variable
-2. con esas variables crear un objeto 
-3. convertir el objeto en string 
-4.  GUardar clavevalor en local storage SET ITEM 
-5. Obtener con getitem con la clave 
-6. convertir a JSON.Parse 
-
-*/
+  1. Obetenr datos del formulario cada uno corresponde a una variable
+  2. con esas variables crear un objeto 
+  3. convertir el objeto en string 
+  4.  GUardar clavevalor en local storage SET ITEM 
+  5. Obtener con getitem con la clave 
+  6. convertir a JSON.Parse 
+  
+  */
 })
