9a0e871491f5e7b4130d03bf57f05eb19de63b8a
Arantxa Vazquez <arantxavazquez177@gmail.com>
2024-07-26 16:49:50 -0600

    enlazar: footer como funcion de js en cada html

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import './src/pages/contacto/paginaContacto.js'
 import './src/pages/avisoPrivacidad/avisoPrivacidad.css'
 import './src/components/navbar/navbar-app'
 import { navbarApp } from './src/components/navbar/navbar-app.js'
+import { footerApp } from './src/components/footer/footer.js' 
 
 document.querySelector("#navbar-app").innerHTML= navbarApp();
-
+document.querySelector("#footer-app").innerHTML= footerApp();
