ee58ca8919eff9da0093ce875213c2555b6894a1
Arantxa Vazquez <arantxavazquez177@gmail.com>
2024-07-26 14:16:45 -0600

    añadir aviso de privacidad

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 import './styles.scss'
 import './src/pages/nosotros/paginaNosotros.css'
 import './src/pages/contacto/paginaContacto.js'
-
+import './src/pages/avisoPrivacidad/avisoPrivacidad.css'
 import './src/components/navbar/navbar-app'
 import { navbarApp } from './src/components/navbar/navbar-app.js'
 
