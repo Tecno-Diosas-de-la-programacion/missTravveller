aa55ed51f1a7b848a713c6960bd5c0ab446e3bf7
Jaqueline <jaqueline.torres.perez.unam@gmail.com>
2024-07-28 21:40:31 -0600

    update: se agregan cambios de estilo y se corrigió la imagen de fondo y las clases en el js del navbar

--- src/components/navbar/navbar-app.js
+++ src/components/navbar/navbar-app.js
@@ -10,24 +10,26 @@ const navbarApp = () => {
   const menuOptions = createListItems(pages, currentHTML);
 
   return `
-  <nav class="navbar navbar-expand-md fixed-top">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/index.html">
-        <img id="logo-missTravveller" src="/img/Logo_MissTravveller.png" alt="Logo" class="d-inline-block align-text-center">
-      </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
-          <span>
-            <img id="hamburguesa" src="/img/hamburguesa.png" alt="Imagen patito despliega menú">
-          </span>
+    <nav class="navbar navbar-expand-lg fixed-top custom-navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/index.html">
+          <img id="logo-missTravveller" src="/img/Logo_MissTravveller.png" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <img id="hamburguesa" src="/img/hamburguesa.png" alt="Imagen patito despliega menú">
         </button>
-      <div class="navbar-collapse collapse show" id="navbarCollapse" style="">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          ${menuOptions}
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ms-auto me-auto mb-2 mb-md-0">
+
+            ${menuOptions}
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+    <div class="spacer"></div>
   `;
 };
+
+
 
 export { navbarApp };
