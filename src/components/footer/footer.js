53249ef7e1379b79821eac696d80784c470f654e
Dulce <dulcam0923@yahoo.com>
2024-08-08 17:21:12 -0600

    Se agrego links a footer mas target

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,17 +8,17 @@ const footerApp = () => {
     
         <ul class="nav col-md-3.5 justify-content-left">
           <li class="foot-item">
-            <a class="footer.redes" href="#">
+            <a class="footer.redes" href="https://www.facebook.com/profile.php?id=61559062033043" target="_blank">
               <i id="footer-facebook" class="bi bi-facebook"></i>
             </a>
           </li>
           <li class="foot-item">
-            <a class="footer.redes" href="https://www.instagram.com/misstraveler.mx?igsh=bnFjbDdsMzkybnVx">
+            <a class="footer.redes" href="https://www.instagram.com/misstraveler.mx?igsh=bnFjbDdsMzkybnVx"target="_blank">
               <i id="footer-instagram" class="bi bi-instagram" width=""></i>
             </a>
           </li>
           <li class="foot-item">
-            <a class="footer.redes" href="#">
+            <a class="footer.redes" href="https://www.tiktok.com/@misstraveler.mx?_t=8oiBgjYc4NJ&_r=1"target="_blank">
               <i id="footer-tiktok" class="bi bi-tiktok"></i>
             </a>
           </li>
