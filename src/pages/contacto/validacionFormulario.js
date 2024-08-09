d7dfe75df33deb8ebc72a2c8d8074ac9f1a6608c
Diana Gallegos <diana_prz21@hotmail.com>
2024-08-08 22:33:43 -0600

    Resolución de error en validación de formulario e inicio de petición post.

--- src/pages/contacto/validacionFormulario.js
+++ src/pages/contacto/validacionFormulario.js
@@ -1,104 +1,108 @@
-const userNameFiled = document.querySelector("[name=nombre]");
-const userEmailFiled = document.querySelector("[name=correo]");
-const userPhoneFiled = document.querySelector("[name=teléfono]");
-const userMessageFiled = document.querySelector("[name=mensaje]");
+document.addEventListener('DOMContentLoaded', function () {
+    const userNameFiled = document.querySelector("[name=nombre]");
+    const userEmailFiled = document.querySelector("[name=correo]");
+    const userPhoneFiled = document.querySelector("[name=telefono]");
+    const userMessageFiled = document.querySelector("[name=mensaje]");
 
-const setError = (field, isError = true) => {
-    if (isError) {
-        field.classList.add("invalid");
-        field.nextElementSibling.classList.add("error");
-        field.nextElementSibling.innerHTML = `El campo ${field.name} es requerido.`;
-    } else {
-        field.classList.remove("invalid");
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerHTML = "";
-    }
-}
-
-const validateEmptyField = (event) => {
-    const field = event.target;
-    const fieldValue = event.target.value;
-    if (fieldValue.trim().length === 0) {
-        setError(field);
-    } else {
-        setError(field, false);
-    }
-};
-
-const validateEmailFormat = (event) => {
-    const field = event.target;
-    const fieldValue = event.target.value;
-
-    const regex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/);
-
-    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
-        field.classList.add("invalid");
-        field.nextElementSibling.classList.add("error");
-        field.nextElementSibling.innerHTML = `Porfavor ingresa un correo electrónico válido.`;
-    } else {
-        field.classList.remove("invalid");
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerHTML = "";
-    }
-};
-
-const validatePhoneFormat = (event) => {
-    const field = event.target;
-    const fieldValue = event.target.value;
-
-    const regex = new RegExp(/^\d{10}$/);
-
-    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {//!false = true
-        field.classList.add("invalid");
-        field.nextElementSibling.classList.add("error");
-        field.nextElementSibling.innerHTML = `Porfavor ingresa un número telefónico válido.`;
-    } else {
-        field.classList.remove("invalid");
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerHTML = "";
-    }
-};
-
-userNameFiled.addEventListener("blur", validateEmptyField);
-userEmailFiled.addEventListener("blur", validateEmptyField);
-userPhoneFiled.addEventListener("blur", validateEmptyField);
-userMessageFiled.addEventListener("blur", validateEmptyField);
-
-userEmailFiled.addEventListener("input", validateEmailFormat);//input se ejecuta un evento
-userPhoneFiled.addEventListener("input", validatePhoneFormat);
-
-
-const form = document.getElementById("contactForm");
-
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Verificar que no haya campos con la clase invalid
-    const invalidFields = document.querySelectorAll(".invalid");
-    if (invalidFields.length > 0) {
-        alert("Por favor, corrige los errores en el formulario.");
+    if (!userNameFiled || !userEmailFiled || !userPhoneFiled || !userMessageFiled) {
+        console.error("Uno o más elementos no se encontraron en el DOM.");
         return;
     }
 
-    // Recopilar datos del formulario
-    const formData = {
-        nombre: userNameFiled.value,
-        correo: userEmailFiled.value,
-        telefono: userPhoneFiled.value,
-        mensaje: userMessageFiled.value
+    const setError = (field, isError = true) => {
+        if (isError) {
+            field.classList.add("invalid");
+            field.nextElementSibling.classList.add("error");
+            field.nextElementSibling.innerHTML = `El campo ${field.name} es requerido.`;
+        } else {
+            field.classList.remove("invalid");
+            field.nextElementSibling.classList.remove("error");
+            field.nextElementSibling.innerHTML = "";
+        }
+    }
+
+    const validateEmptyField = (event) => {
+        const field = event.target;
+        const fieldValue = event.target.value;
+        if (fieldValue.trim().length === 0) {
+            setError(field);
+        } else {
+            setError(field, false);
+        }
     };
 
-    // Enviar correo electrónico usando EmailJS
-    emailjs.send("service_3np4zfy", "template_ofvcspn", formData)
-        .then(response => {
-            swal('','Tu mensaje fue enviado con éxito','success');
-        }, error => {
-            swal('','Hubo un error, inténtalo nuevamente','error');
-        });
+    const validateEmailFormat = (event) => {
+        const field = event.target;
+        const fieldValue = event.target.value;
+
+        const regex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/);
+
+        if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+            field.classList.add("invalid");
+            field.nextElementSibling.classList.add("error");
+            field.nextElementSibling.innerHTML = `Porfavor ingresa un correo electrónico válido.`;
+        } else {
+            field.classList.remove("invalid");
+            field.nextElementSibling.classList.remove("error");
+            field.nextElementSibling.innerHTML = "";
+        }
+    };
+
+    const validatePhoneFormat = (event) => {
+        const field = event.target;
+        const fieldValue = event.target.value;
+
+        const regex = new RegExp(/^\d{10}$/);
+
+        if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {//!false = true
+            field.classList.add("invalid");
+            field.nextElementSibling.classList.add("error");
+            field.nextElementSibling.innerHTML = `Porfavor ingresa un número telefónico válido.`;
+        } else {
+            field.classList.remove("invalid");
+            field.nextElementSibling.classList.remove("error");
+            field.nextElementSibling.innerHTML = "";
+        }
+    };
+
+    userNameFiled.addEventListener("blur", validateEmptyField);
+    userEmailFiled.addEventListener("blur", validateEmptyField);
+    userPhoneFiled.addEventListener("blur", validateEmptyField);
+    userMessageFiled.addEventListener("blur", validateEmptyField);
+
+    userEmailFiled.addEventListener("input", validateEmailFormat);//input se ejecuta un evento
+    userPhoneFiled.addEventListener("input", validatePhoneFormat);
+
+
+    const form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        // Verificar que no haya campos con la clase invalid
+        const invalidFields = document.querySelectorAll(".invalid");
+        if (invalidFields.length > 0) {
+            alert("Por favor, corrige los errores en el formulario.");
+            return;
+        }
+
+        // Recopilar datos del formulario
+        const formData = {
+            nombre: userNameFiled.value,
+            correo: userEmailFiled.value,
+            telefono: userPhoneFiled.value,
+            mensaje: userMessageFiled.value
+        };
+
+        try {
+            await postRegisterForm(formData);
+            alert("Gracias por tu mensaje");
+            document.reset();
+          } catch (error) {
+            console.log("Error al enviar a base de datos.");
+          }
+
+    });
+
 });
 
-(function () {
-    emailjs.init({
-        publicKey: "bpYdmZh4wjxCLzdkz",
-    });
-})();
