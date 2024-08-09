3e4e29cbb8257c7a3494f19e9f31032965aef63e
Abigail Ramirez <abigailrg03@gmail.com>
2024-08-08 22:30:39 -0600

    se añaden js de domain name y post registro, se agregan funciones fetch tambien

---
+++ src/components/api/postRegistro.js
@@ -0,0 +1,25 @@
+import { domainName } from "../../components/api/domainName.js"
+
+const postRegisterForm = async ( newUserData ) => {
+    const url = `${domainName()}/api/v1/users`;
+  
+    const options = { //Aqui se comunica el front con el back
+      method: "POST", // DELETE, PUT, POST, GET
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newUserData),
+    };
+  
+    const response = await fetch(url, options);
+  
+    if( response.status !== 201){
+      const serverError = await response.json();
+      throw serverError.message;
+    }
+  
+    const registerResult = await response.json();
+    console.log(registerResult);
+  
+    return registerResult;
+  };
+  
+  export { postRegisterForm };
