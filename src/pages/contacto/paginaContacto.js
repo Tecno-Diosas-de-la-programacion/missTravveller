672d00f875cc9f5ee8653b8472ce399c8b91e955
Fanny Contreras <fanny7cg@gmail.com>
2024-08-08 18:51:18 -0600

    Se creó: una nueva rama y se colocó un preloader a cada una de las páginas y se cambió el logo de la pestaña por el pato

--- src/pages/contacto/paginaContacto.js
+++ src/pages/contacto/paginaContacto.js
@@ -3,3 +3,16 @@ import './paginaContacto.css'
 import { navbarApp } from '../../components/navbar/navbar-app.js'
 
 document.querySelector("#navbar-app").innerHTML= navbarApp();
+
+// preloader 
+
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    const contenido = document.getElementById('contenido');
+    
+    // Simula un tiempo de carga antes de mostrar el contenido
+    setTimeout(() => {
+        preloader.style.display = 'none';
+        contenido.style.display = 'block';
+    }, 3000); // Cambia 3000 por el tiempo que desees en milisegundos
+});
--- src/pages/nosotros/paginaNosotros.js
+++ src/pages/nosotros/paginaNosotros.js
@@ -6,3 +6,16 @@ import { navbarApp } from '../../components/navbar/navbar-app.js'
 
 
 document.querySelector("#navbar-app").innerHTML= navbarApp();
+
+// preloader 
+
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    const contenido = document.getElementById('contenido');
+    
+    // Simula un tiempo de carga antes de mostrar el contenido
+    setTimeout(() => {
+        preloader.style.display = 'none';
+        contenido.style.display = 'block';
+    }, 3000); // Cambia 3000 por el tiempo que desees en milisegundos
+});
